c2375f6ec900c43b2bcd0d6696a683b66e943426
Stajyer <stajyer@tp.com>
2026-08-17 10:02:18 +0300

    fix(js): resolve syntax error in escapeHTML regex

--- fastapi_app/static/main.js
+++ fastapi_app/static/main.js
@@ -5,7 +5,7 @@ function escapeHTML(str) {
         .replace(/&/g, '&amp;')
         .replace(/</g, '&lt;')
         .replace(/>/g, '&gt;')
-        .replace(///g, '&quot;')
+        .replace(/"/g, '&quot;')
         .replace(/'/g, '&#039;');
 }
 async function apiFetch(url, options = {}) {
